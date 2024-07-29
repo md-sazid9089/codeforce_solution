8550a544ce20cd4f15fbd80f50ba87b6f3af0b66
Syed Mohammed Sazid Ullah <164257013+md-sazid9089@users.noreply.github.com>
2024-07-29 22:53:03 +0600

    Add files via upload

---
+++ 96A_Football.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main() {
+    optimize();
+    string s;
+    cin >> s;
+    int count = 1;
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] == s[i - 1]) {
+            count++;
+            if (count >= 7) {
+                cout << "YES";
+                return 0;
+            }
+        } else {
+            count = 1;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
