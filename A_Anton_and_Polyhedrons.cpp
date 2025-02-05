5155d55531871464344308d5c60f24355fb75683
Syed Mohammed Sazid Ullah <164257013+md-sazid9089@users.noreply.github.com>
2025-02-05 23:29:14 +0600

    Add files via upload

---
+++ A_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  main(){
+int n;
+cin>>n;
+int sum=0;
+while(n--){
+    string s;
+    cin>>s;
+    if(s[0]=='C')sum+=6;
+        else if(s[0]=='T')sum=sum+4;
+        else if(s[0]=='O')sum=sum+8;
+        else if(s[0]=='D')sum=sum+12;
+        else sum=sum+20;
+}
+cout<<sum<<endl;
+return 0;
+}
